0353873c650026d8117f55494866e2ab9be9d06c
guixuan <wangguixuan@qjcdss.com>
2021-12-28 18:37:43 +0800

    feat(bert_crf.py) 优化NER模型

--- ner/tagging/models/bert_crf.py
+++ ner/tagging/models/bert_crf.py
@@ -8,15 +8,19 @@
 '''
 import torch
 from overrides import overrides
-from typing import Dict, List
+from torch.nn import Linear
+from typing import Dict, List, Optional
 from allennlp.data.vocabulary import Vocabulary
 from transformers.models.bert.modeling_bert import BertModel
 from transformers.models.bert.configuration_bert import BertConfig
 from allennlp.models.model import Model
-from allennlp.modules.text_field_embedders import  TextFieldEmbedder
+from allennlp.modules.seq2seq_encoders import Seq2SeqEncoder
+from allennlp.modules.text_field_embedders import TextFieldEmbedder
+from allennlp.modules.time_distributed import TimeDistributed
+from allennlp.modules.feedforward import FeedForward
 from allennlp.nn.initializers import InitializerApplicator
 from allennlp.nn import util
-from allennlp.modules.conditional_random_field import ConditionalRandomField
+from allennlp.modules.conditional_random_field import ConditionalRandomField, allowed_transitions
 from allennlp.nn.util import get_text_field_mask, sequence_cross_entropy_with_logits
 from allennlp.training.metrics import CategoricalAccuracy, SpanBasedF1Measure
 import torch.nn.functional as F
@@ -25,23 +29,52 @@ import torch.nn.functional as F
 @Model.register('bert_crf')
 class BertCrf(Model):
     def __init__(self, vocab: Vocabulary,
-                 bert_path: str,
                  embedder: TextFieldEmbedder,
-                 model_type:str = 'bert',
+                 encoder: Optional[Seq2SeqEncoder] = None,
+                 constraint_crf_decoding: bool = True,
+                 feedforward: Optional[FeedForward] = None,
+                 dropout: Optional[float] = None,
+                 label_namespace: str = 'labels',
+                 use_pretrained_model: Optional[bool] = None,
+                 label_encoding: str = 'BIO',
+                 use_crf: bool = True,
                  initializer: InitializerApplicator = InitializerApplicator(),
                  **kwargs):
         super().__init__(vocab, **kwargs)
 
-
-        # self._bert_model = BertModel.from_pretrained(bert_path)
+        self._num_tags = vocab.get_vocab_size('labels')
         self._embedder = embedder
 
-        self._classifier = torch.nn.Linear(
-            in_features=self._embedder.get_output_dim(), out_features=vocab.get_vocab_size('labels'))
+        self._encoder = encoder
+        self._constrain_crf_decoding = constraint_crf_decoding
+        self._use_pretrained_model = use_pretrained_model
+        self._use_crf = use_crf
+        self._label_encoding = label_encoding
+        self._label_namespace = label_namespace
 
-        self._crf = ConditionalRandomField(vocab.get_vocab_size('labels'))
+        if dropout:
+            self._dropout = torch.nn.Dropout(dropout)
+        else:
+            self._dropout = None
 
-        self._f1 = SpanBasedF1Measure(vocab, 'labels')
+        self._feedforward = feedforward
+
+        if self._feedforward:
+            output_dim = self._feedforward.get_output_dim()
+        else:
+            output_dim = self._embedder.get_output_dim()
+
+        self._classifier = TimeDistributed(Linear(in_features=output_dim, out_features=self._num_tags))
+
+        if self._use_crf:
+            if constraint_crf_decoding:
+                labels = vocab.get_index_to_token_vocabulary(self._label_namespace)
+                constraints = allowed_transitions(self._label_encoding, labels)
+            else:
+                constraints = None
+            self._crf = ConditionalRandomField(vocab.get_vocab_size('labels'), constraints)
+
+        self._f1 = SpanBasedF1Measure(vocab, self._label_namespace)
 
         initializer(self)
 
@@ -63,13 +96,13 @@ class BertCrf(Model):
 
         def decode_tags(tags):
             return [
-                self.vocab.get_token_from_index(tag, namespace='labels') for tag in tags
+                self.vocab.get_token_from_index(tag, namespace=self._label_namespace) for tag in tags
             ]
 
         output_dict["tags"] = [decode_tags(t) for t in output_dict["tags"]]
         return output_dict
 
-    def forward(self, tokens: Dict[str, torch.Tensor], labels: torch.Tensor = None) -> Dict[str, torch.Tensor]:
+    def forward(self, tokens: Dict[str, Dict[str, torch.Tensor]], labels = None) -> Dict[str, torch.Tensor]:
         # mask = tokens['tokens']['mask']
         # bert_embeddings, _ = self._bert_model(
         #     input_ids=tokens['tokens']['token_ids'],
@@ -78,36 +111,44 @@ class BertCrf(Model):
         #     return_dict=False,
         # )
         mask = get_text_field_mask(tokens)
+        tmp_encoded = self._embedder(tokens)
+        if self._encoder:
+            tmp_encoded = self._encoder(tmp_encoded, mask)
 
-        embedded = self._embedder(tokens)
+        if self._feedforward:
+            tmp_encoded = self._feedforward(tmp_encoded)
 
-        # encoded = self._encoder(embedded, mask)
+        if self._dropout:
+            tmp_encoded = self._feedforward(tmp_encoded)
+        classified = self._classifier(tmp_encoded)
+        if self._use_crf:
 
-        classified = self._classifier(embedded)
-        # class_probabilities = F.softmax(classified, dim=-1)
-        # output = {"logits": classified, "class_probabilities": class_probabilities}
-        # if labels is not None:
-        #     loss = sequence_cross_entropy_with_logits(classified, labels, mask)
-        #     output['loss'] = loss
-        #     self._f1(classified, labels, mask)
+            viterbi_tags = self._crf.viterbi_tags(classified, mask)
+            viterbi_tags = [path for path, score in viterbi_tags]
 
-        viterbi_tags = self._crf.viterbi_tags(classified, mask)
-        viterbi_tags = [path for path, score in viterbi_tags]
+            # Just get the top tags and ignore the scores.
+            # predicted_tags = cast(List[List[int]], [x[0][0] for x in viterbi_tags])
 
-        # Just get the top tags and ignore the scores.
-        # predicted_tags = cast(List[List[int]], [x[0][0] for x in viterbi_tags])
+            broadcasted = self._broadcast_tags(viterbi_tags, classified)
 
-        broadcasted = self._broadcast_tags(viterbi_tags, classified)
+            output = {
+                'logits': classified,
+                'tags': viterbi_tags
+            }
 
-        output = {
-            'logits': classified,
-            'tags': viterbi_tags
-        }
+            if labels is not None:
+                log_likelihood = self._crf(classified, labels, mask)
 
-        if labels is not None:
-            log_likelihood = self._crf(classified, labels, mask)
+                output['loss'] = -log_likelihood
+                self._f1(broadcasted, labels, mask)
 
-            output['loss'] = -log_likelihood
-            self._f1(broadcasted, labels, mask)
+
+        else:
+            class_probabilities = F.softmax(classified, dim=-1)
+            output = {"logits": classified, "class_probabilities": class_probabilities}
+            if labels is not None:
+                loss = sequence_cross_entropy_with_logits(classified, labels, mask)
+                output['loss'] = loss
+                self._f1(classified, labels, mask)
 
         return output
