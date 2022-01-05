1f4ca5d609822a510bdb85fe764dcedd09a61b32
guixuan <wangguixuan@qjcdss.com>
2022-01-05 18:33:36 +0800

    feat(mrc_f1_measure): 增加部分函数

--- ner/tagging/metrics/mrc_f1_measure.py
+++ ner/tagging/metrics/mrc_f1_measure.py
@@ -1,8 +1,76 @@
-#!/usr/bin/env python
-# -*- coding: UTF-8 -*-
 '''
 @Project ：ner 
 @File    ：mrc_f1_measure.py
 @Author  ：hbx
 @Date    ：2022/1/5 16:40 
 '''
+import torch
+from allennlp.training.metrics import Metric
+from overrides import overrides
+from typing import Dict, List, Any, Set
+from collections import defaultdict
+
+@Metric.register('mrc_f1')
+class MRCF1Measure(Metric):
+
+    def __init__(self):
+        self._true_positives: Dict[str, int] = defaultdict(int)
+        self._false_positives: Dict[str, int] = defaultdict(int)
+        self._false_negatives: Dict[str, int] = defaultdict(int)
+
+    @overrides
+    def __call__(
+            self,
+            start_predictions: torch.Tensor,
+            end_predictions: torch.Tensor,
+            gold_start_labels: torch.Tensor,
+            gold_end_labels: torch.Tensor,
+        ):
+        pass
+
+
+    @overrides
+    def get_metric(self, reset: bool) -> Dict[str, Any]:
+        all_tags: Set[str] = set()
+        all_tags.update(self._true_positives.keys())
+        all_tags.update(self._false_positives.keys())
+        all_tags.update(self._false_negatives.keys())
+        all_metrics = {}
+        for tag in all_tags:
+            precision, recall, f1_measure = self._compute_metrics(
+                self._true_positives[tag], self._false_positives[tag], self._false_negatives[tag]
+            )
+            precision_key = "precision" + "-" + tag
+            recall_key = "recall" + "-" + tag
+            f1_key = "f1-measure" + "-" + tag
+            all_metrics[precision_key] = precision
+            all_metrics[recall_key] = recall
+            all_metrics[f1_key] = f1_measure
+
+        # Compute the precision, recall and f1 for all spans jointly.
+        precision, recall, f1_measure = self._compute_metrics(
+            sum(self._true_positives.values()),
+            sum(self._false_positives.values()),
+            sum(self._false_negatives.values()),
+        )
+        all_metrics["precision-overall"] = precision
+        all_metrics["recall-overall"] = recall
+        all_metrics["f1-measure-overall"] = f1_measure
+        if reset:
+            self.reset()
+        return all_metrics
+
+    @overrides
+    def reset(self) -> None:
+        self._true_positives = defaultdict(int)
+        self._false_positives = defaultdict(int)
+        self._false_negatives = defaultdict(int)
+
+    @staticmethod
+    def _compute_metrics(true_positives: int, false_positives: int, false_negatives: int):
+        precision = true_positives / (true_positives + false_positives + 1e-13)
+        recall = true_positives / (true_positives + false_negatives + 1e-13)
+        f1_measure = 2.0 * (precision * recall) / (precision + recall + 1e-13)
+        return precision, recall, f1_measure
+
+
