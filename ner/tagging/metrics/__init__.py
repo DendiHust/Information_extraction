3cd1f79b4846d5dbed5ebbc75e1e138188c040c4
guixuan <wangguixuan@qjcdss.com>
2022-01-05 16:55:23 +0800

    feat(mrc_f1_measure)：参考SpanBasedF1Measure实现MRC模型的评估，未完成；

---
+++ ner/tagging/metrics/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+'''
+@Project ：ner 
+@File    ：__init__.py.py
+@Author  ：hbx
+@Date    ：2022/1/5 16:39 
+'''
---
+++ ner/tagging/metrics/mrc_f1_measure.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+'''
+@Project ：ner 
+@File    ：mrc_f1_measure.py
+@Author  ：hbx
+@Date    ：2022/1/5 16:40 
+'''
